88563cddb3bf6a4f61dbba445b745d27e4eb37d3
rajalishikhe <nahidujjamanraj@gmail.com>
2025-04-07 13:09:58 +0600

    Find the length of List using Function

---
+++ ps18.py
@@ -0,0 +1,7 @@
+# WAF to print the length of a list. (list is the parameter) 
+myFriend=['Arif',"Jihat","Rasel","Nibir","Siam","My best Friend My wife Bushra"]
+def find_Length_List(a):
+    x=len(a)
+    print(x)
+
+find_Length_List(myFriend)
