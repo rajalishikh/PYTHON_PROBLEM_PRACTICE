754faafe66661ce557bb9f20fb7113df55170a2d
rajalishikhe <nahidujjamanraj@gmail.com>
2025-04-09 11:21:11 +0600

    I make a family  table dor database

---
+++ ps21.py
@@ -0,0 +1,8 @@
+def find_event_odd():
+    z=int(input("Please give me a parameter"))
+   
+    if(z % 2== 0):
+        print("The number is event number")
+    elif z % 2==1:
+        print("The number is odd number ")
+find_event_odd()
